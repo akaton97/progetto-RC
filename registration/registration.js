79e9ac16d21fd1444667c916f93bb3f32e1fa98c
Unknown <plutoboy97@gmail.com>
2019-04-12 11:09:30 +0200

    finito tutto il login
    devo implementare ancora il log-out, però dovremmo stare apposto per ora. SOno riuscito a far runnare il server apache

--- registration/registration.js
+++ registration/registration.js
@@ -1,6 +1,6 @@
 
   function onFailure(error) {
-    console.log(error);
+    alert(error)
 }
 
 function onSignIn(googleUser) {
@@ -14,4 +14,19 @@ function onSignIn(googleUser) {
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
+}
+
+var GoogleAuth; // Google Auth object.
+function initClient() {
+  gapi.client.init({
+      'apiKey': 'AIzaSyCYdv78ZfcuX8aJn7wDZVlQeRs6bBHUhes',
+      'clientId': '396784371487-8nfv15leuls7jtlh1mtp6a7eevsj67om.apps.googleusercontent.com',
+      'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest']
+  }).then(function () {
+      GoogleAuth = gapi.auth2.getAuthInstance();
+
+      // Listen for sign-in state changes.
+      GoogleAuth.isSignedIn.listen(updateSigninStatus);
+  });
 }
