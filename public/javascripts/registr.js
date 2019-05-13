838f0341803265de7f68cdf36feedf3af7899314
Lorenzo Scarlino <lorenzo.scarlino.98@gmail.com>
2019-05-13 17:49:26 +0200

    Fix

--- public/javascripts/registr.js
+++ public/javascripts/registr.js
@@ -45,9 +45,8 @@ function inserisciReg() {  //RISOLTO.
     }
     u[nextpos]=o;
     localStorage.reg = JSON.stringify(u);
-    alert("Iscrizione effettuata con successo");
-    //swal("Good job", "Iscrizione effettuata con successo!", "success")
-    alert("verrai reindirizzato alla pagina di login");
+    //swal("Good job", "Iscrizione effettuata con successo! Verrai reindirizzato alla pagina di login", "success")
+    alert("Iscrizione effettuata con successo! Verrai reindirizzato alla pagina di login");
     return true;
 }  
 
