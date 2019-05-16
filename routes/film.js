405cf6317ddc51e6f3acd4e9604480491003af94
Lorenzo Scarlino <lorenzo.scarlino.98@gmail.com>
2019-05-16 13:58:09 +0200

    Fix API

--- routes/film.js
+++ routes/film.js
@@ -1,16 +1,19 @@
 var express = require("express");
-const MovieDB = require('moviedb')('807244b48f548385eab5d123309248e6');
 var router = express.Router();
+var request = require("request");
 
 router.get('/', function(req,res) {
-	res.render('film', {title: 'Express'});
-	MovieDB.movieInfo({ id: 666}, (err, res) => {
-		if(err) {console.log('err')}
-		console.log(res);
-		var s = res;
+	let url = 'https://api.themoviedb.org/3/movie/5?api_key=807244b48f548385eab5d123309248e6&language=it&append_to_response=hello';
+  request(url, function(err,resp,body){
+    if(err) console.log("err");
+    else{
 		
-		
-	  });
+      res.json(JSON.parse(body))
+    }
+  })
+
 });
+
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('homepage', { title: 'Express' });
-});
 
+
+router.get("/", function(req,res){
+  res.render("homepage", {title: "Express"})
+})
 module.exports = router;
