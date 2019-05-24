15bf5bbd85de19e52b433d57c723542799d51fc1
Daniele <michelori.1757243@studenti.uniroma1.it>
2019-05-24 17:46:59 +0200

    Chat
    per far funzionare avviare sul terminale il file .js tramite comadno: node index.js

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,30 +1,30 @@
 const connection = new WebSocket('ws://localhost:12345'),
-    box = document.getElementById('box'),
-    msg = document.getElementById('msg');
+  box = document.getElementById('box'),
+  msg = document.getElementById('msg');
 
 connection.addEventListener('open', () => {
-    console.log('connected');
+  console.log('connected');
 });
 
 connection.addEventListener('message', e => {
-   let p = document.createElement('p');
-   p.textContent = e.data;
-   box.appendChild(p);
+  let p = document.createElement('p');
+  p.textContent = e.data;
+  box.appendChild(p);
 });
 
 function send (data) {
-    if(connection.readyState === WebSocket.OPEN) {
-        connection.send(data);
-    } else {
-        throw 'Not connected';
-    }
+  if(connection.readyState === WebSocket.OPEN) {
+    connection.send(data);
+  } else {
+      throw 'Not connected';
+  }
 }
 
 msg.addEventListener('keydown', e => {
-    let kc = e.which || e.keyCode;
+  let kc = e.which || e.keyCode;
 
-    if(kc === 13) {
-        send(msg.value);
-        msg.value = '';
-    }
+  if(kc === 13) {
+    send(msg.value);
+    msg.value = '';
+  }
 });
