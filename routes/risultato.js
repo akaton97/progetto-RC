37570b9b870ce3b279cd1db46451b3aa20a06ecc
Lorenzo Scarlino <lorenzo.scarlino.98@gmail.com>
2019-05-27 16:11:01 +0200

    problema array json

--- routes/risultato.js
+++ routes/risultato.js
@@ -7,7 +7,7 @@ router.get('/:id?', function(req,res) {
 			let id = req.params.id.substring(1);
 			console.log(id)
 			let url = "https://api.themoviedb.org/3/movie/"+
-			id	+"?api_key=807244b48f548385eab5d123309248e6&language=en-US";
+			id	+"?api_key=807244b48f548385eab5d123309248e6&language=it-IT";
 			request(url, function(err, response, body) {
 				if (err) {
 					res.render("films", alert("pio"));
