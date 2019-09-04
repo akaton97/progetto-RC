b24e2d30ba877ee944b3c25292a4b2459a3bd652
Daniele <michelori.1757243@studenti.uniroma1.it>
2019-09-04 18:00:35 +0200

    modifiche

---
+++ routes/chatroom.js
@@ -0,0 +1,9 @@
+//SERVER
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('ChatRoom', { title: 'Express' }); //mostra una pagina HTML, cerca search nella /views e lo mostra
+});
+
+module.exports = router;
