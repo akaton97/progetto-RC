8798895b3eeb14855540aac225a5691d4f39f3e9
Lorenzo Scarlino <lorenzo.scarlino.98@gmail.com>
2019-10-12 19:59:33 +0200

    fixini

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const {ensureAuthenticated} = require('../public/javascript/authControl');
+
 router.get("/", function(req,res){
   res.render("homepage", {user: req.user});
 })
