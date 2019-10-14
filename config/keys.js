cbaecca841cfa3d5de4d11ebc0ca8b5822f1eac0
Unknown <plutoboy97@gmail.com>
2019-10-14 13:13:22 +0200

    fix google

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 //add this to .gitignore
 module.exports = {
-        google_clientID: '396784371487-8nfv15leuls7jtlh1mtp6a7eevsj67om.apps.googleusercontent.com',
-        google_clientSecret: 'leg4yuqSL7MlfFHlkSAqq70P',
+        google_clientID: '396784371487-2m46oh59bgo72f75c239qhu024vhcfho.apps.googleusercontent.com',
+        google_clientSecret: 'qQDPByQ_8smIWqAKLtlKCOe5',
         mongoUri: 'mongodb+srv://Akaton:M3PpiUtDtM3ak4Ts@moviekraken-3ambv.azure.mongodb.net/test?retryWrites=true&w=majority',
 }
