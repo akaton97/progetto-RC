38170e8c1a8e71ad9780c79ceb71a5c3d41a5140
Unknown <plutoboy97@gmail.com>
2019-10-14 13:00:00 +0200

    fix heroku 2.0

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 //add this to .gitignore
 module.exports = {
-        google_clientID: '396784371487-umfe7k76vtb228htm2gmp4mucgj9bdbg.apps.googleusercontent.com',
-        google_clientSecret: 'kahuEAEZ6kccpf_WBiwgso5e',
+        google_clientID: '396784371487-8nfv15leuls7jtlh1mtp6a7eevsj67om.apps.googleusercontent.com',
+        google_clientSecret: 'leg4yuqSL7MlfFHlkSAqq70P',
         mongoUri: 'mongodb+srv://Akaton:M3PpiUtDtM3ak4Ts@moviekraken-3ambv.azure.mongodb.net/test?retryWrites=true&w=majority',
 }
