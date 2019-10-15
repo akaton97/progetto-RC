5a0a4a39526324f8007269ebf6d2de63cb326dae
Unknown <plutoboy97@gmail.com>
2019-10-15 11:00:47 +0200

    chi ritocca le key è un beo

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 //add this to .gitignore
 module.exports = {
-        google_clientID: '620881066128-jq0d94l0t73g9voil59mhjto0hh5mvpm.apps.googleusercontent.com',
-        google_clientSecret: 'IRLFmB9LZVHzObLzvqjYZ42D',
+        google_clientID: '396784371487-2m46oh59bgo72f75c239qhu024vhcfho.apps.googleusercontent.com',
+        google_clientSecret: 'qQDPByQ_8smIWqAKLtlKCOe5',
         mongoUri: 'mongodb+srv://Akaton:M3PpiUtDtM3ak4Ts@moviekraken-3ambv.azure.mongodb.net/test?retryWrites=true&w=majority',
 }
